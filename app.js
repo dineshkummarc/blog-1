de9208a65cac719a47e3b1a6230472250a9ca198
Michael Lam <treadsafely@gmail.com>
2012-04-29 11:34:48 +0100

    use process.env.PORT if exists

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // Just a basic server setup for this site
 var Stack = require('stack'),
     Creationix = require('creationix'),
-    Http = require('http');
+    Http = require('http'),
+    port = process.env.PORT || 3000;
 
 Http.createServer(Stack(
   Creationix.log(),
   require('wheat')(__dirname)
-)).listen(3000);
+)).listen(port);
